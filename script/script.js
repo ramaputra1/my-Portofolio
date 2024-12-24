a9613829edb6236d8b1d3a96f00215478ab6cd0e
ramaputra1 <166339284+ramaputra1@users.noreply.github.com>
2024-12-23 18:54:17 -1000

    rebuild layout for responsiveness + add animation cursor

--- script/script.js
+++ script/script.js
@@ -1,3 +1,13 @@
+// cursor
+const cursor = document.querySelector(".cursor");
+
+document.addEventListener("mousemove", (e) => {
+  cursor.setAttribute(
+    "style",
+    "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;"
+  );
+});
+
 function updateClock() {
   const clockElement = document.getElementById("clock");
   const now = new Date();
